3f7c86e8cae57ecb63720c363d527b5eba8ab1c6
Tanya <REINACREATES1@GMAIL.com>
2022-04-21 22:13:20 -0400

    dna to rna

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function DNAtoRNA(dna) {
+  // Create a function which returns an RNA sequence from the given DNA sequence
+} if (dna){
+  return RNA
+}
+
+console.log(DNAtoRNA);
+function DNAtoRNA (dna) {
+  return dna.replace(/T/g, 'U')
+}
+
